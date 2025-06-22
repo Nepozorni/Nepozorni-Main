497b7ef13266bfe045bc4d30c366e66555be2171
Matic Jurač <juracmatic@gmail.com>
2025-06-22 23:04:41 +0200

    Added CI better testing

---
+++ Tests/ci_tests.py
@@ -0,0 +1,35 @@
+import pytest
+from evaluate import evaluate, is_attentive
+from run_model import *
+from PIL import Image
+import numpy as np
+import os
+
+def test_evaluate_logic_changes():
+    # Different predictions should reset counter
+    result1 = evaluate("Box1", "one_hand", 0, 0)
+    result2 = evaluate("Box1", "no_hands", 3, 3)
+    assert isinstance(result1, float)
+    assert isinstance(result2, float)
+    assert 0 <= result1 <= 100
+    assert 0 <= result2 <= 100
+
+def test_is_attentive_threshold():
+    assert is_attentive(80.0) is True
+    assert is_attentive(49.0) is False
+
+def test_run_model_on_dummy_image():
+    dummy_image = np.zeros((360, 640, 3), dtype=np.uint8)  # black image
+    label, output = run_model("./Models/model-21-05-2025.pt", image=dummy_image)
+    assert isinstance(label, str)
+    assert isinstance(output, str)
+    assert len(label) > 0  # model should return something
+    assert "Inference time" in output
+
+def test_run_model_on_file():
+    if not os.path.exists("sample.png"):
+        pytest.skip("sample.jpg not available for test")
+
+    label, output = run_model("./Models/model-21-05-2025.pt", image_path="sample.png")
+    assert isinstance(label, str)
+    assert "Inference time" in output
